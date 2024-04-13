c2b9fe29ee9791c36331fe537d6c0e690bd278c8
srinivasasai45 <146947289+srinivasasai45@users.noreply.github.com>
2024-04-13 12:49:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ bot/__main__.py
@@ -0,0 +1,66 @@
+import random
+import logging
+from time import sleep
+import traceback
+
+from pyrogram import filters
+
+from bot import app, monitored_chats, chats_map, sudo_users
+from pyrogram.types import Message
+from pyrogram.enums import ParseMode
+from pyrogram import Client
+
+logging.info("Bot Started")
+
+
+@app.on_message(filters.chat(monitored_chats) & filters.incoming)
+def work(_:Client, message:Message):
+    caption = None
+    msg = None
+    chat = chats_map.get(message.chat.id)
+    if chat.get("replace"):
+        for old, new in chat["replace"].items():
+            if message.media and not message.poll:
+                caption = message.caption.markdown.replace(old, new)
+            elif message.text:
+                msg = message.text.markdown.replace(old, new)
+    try:
+        for chat in chat["to"]:
+            if caption:
+                message.copy(chat, caption=caption, parse_mode=ParseMode.MARKDOWN)
+            elif msg:
+                app.send_message(chat, msg, parse_mode=ParseMode.MARKDOWN)
+            else:
+                message.copy(chat)
+    except Exception as e:
+        logging.error(f"Error while sending message from {message.chat.id} to {chat}: {e}")
+
+
+@app.on_message(filters.user(sudo_users) & filters.command(["fwd", "forward"]), group=1)
+def forward(client:Client, message:Message):
+    if len(message.command) > 1 and message.command[1].isdigit():
+        chat_id = int(message.command[1])
+        if chat_id:
+            try:
+                offset_id = 0
+                limit = 0
+                if len(message.command) > 2:
+                    limit = int(message.command[2])
+                if len(message.command) > 3:
+                    offset_id = int(message.command[3])
+                for msg in client.get_chat_history(chat_id, limit=limit, offset_id=offset_id):
+                    msg.copy(message.chat.id)
+                    sleep(random.randint(1, 5))
+            except Exception as e:
+                message.reply_text(f"Error:\n```{traceback.format_exc()}```")
+        else:
+            message.reply_text(
+                "Invalid Chat Identifier. Give me a chat id."
+            )
+    else:
+        message.reply_text(
+            "Invalid Command\nUse /fwd {chat_id} {limit} {first_message_id}"
+        )
+
+
+app.run()
---
+++ bot/helper/utils.py
@@ -0,0 +1,45 @@
+def parse_chats(chats):
+    """
+
+    [[chats]]
+    from = "123456789"
+    to = "123456789"
+
+    [[chats]]
+    from = ["123456789", "123456789"]
+    to = "123456789"
+
+    [[chats]]
+    from = "123456789"
+    to = ["123456789", "123456789"]
+
+    [[chats]]
+    from = ["123456789", "123456789"]
+    to = ["123456789", "123456789"]
+    """
+
+    # Set all from values to a list and, from => [to, replace] in dict
+    monitored_chats = set()
+    chats_map = {}
+
+    for chat in chats:
+        from_chats = chat["from"]
+        to_chats = chat["to"]
+        replace = chat.get("replace")
+
+        if not isinstance(from_chats, list):
+            from_chats = [from_chats]
+
+        if not isinstance(to_chats, list):
+            to_chats = [to_chats]
+
+        for from_chat in from_chats:
+            if from_chat not in chats_map:
+                chats_map[from_chat] = {"to": set(), "replace": replace}
+
+            for to_chat in to_chats:
+                chats_map[from_chat]['to'].add(to_chat)
+            
+            monitored_chats.add(from_chat)
+
+    return list(monitored_chats), chats_map
